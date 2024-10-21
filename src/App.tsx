aa1b516a4ff57a160c91c993cbeea5376a7e21b7
kamasd <kamilkrasicki1982@gmail.com>
2024-10-21 20:24:39 +0200

    started adding SearchBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from "./Navbar.tsx";
+import SearchBar from "@/SearchBar.tsx";
 
 function App() {
 
   return (
     <>
         <Navbar/>
+        <SearchBar/>
     </>
   )
 }
